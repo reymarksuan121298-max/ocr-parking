99e5f22a1a0eb7756969c936b5815a8937aaec48
reymarksuan121298-max <reymarksuan121298@gmail.com>
2026-08-18 15:41:58 +0800

    feat: initialize React Native app entry point in index.js

--- parking-app/index.js
+++ parking-app/index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-import "react-native-gesture-handler"; // must be the first import in the entry file
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
+import 'react-native-url-polyfill/auto';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
